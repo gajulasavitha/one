53c4c354c8685194ca19655c8f50a99b0bc86be5
zxBCN Srinivas <sindhuja.srinivas.ext@boehringer-ingelheim.com>
2022-10-13 18:54:10 +0530

    first commit

---
+++ SSODemo/Home.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SSODemo
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
+
+            //You get the user's first and last name below:
+            string Name = userClaims?.FindFirst("name")?.Value;
+
+            // The 'preferred_username' claim can be used for showing the username
+            string Username = userClaims?.FindFirst("preferred_username")?.Value;
+
+            // The subject/ NameIdentifier claim can be used to uniquely identify the user across the web
+            string Subject = userClaims?.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+
+            // TenantId is the unique Tenant Id - which represents an organization in Azure AD
+            string TenantId = userClaims?.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid")?.Value;
+        }
+    }
+}
---
+++ SSODemo/login.aspx.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace SSODemo
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.IsAuthenticated)
+            {
+                SignIn();
+            }
+            else
+            {
+                var name = System.Security.Claims.ClaimsPrincipal.Current.FindFirst("name").Value;
+            }
+        }
+
+        /// <summary>
+        /// Send an OpenID Connect sign-in request.
+        /// Alternatively, you can just decorate the SignIn method with the [Authorize] attribute
+        /// </summary>
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                System.Web.HttpContext.Current.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties { RedirectUri = "/" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        /// <summary>
+        /// Send an OpenID Connect sign-out request.
+        /// </summary>k
+        /// </summary>k
+        public void SignOut()
+        {
+            System.Web.HttpContext.Current.GetOwinContext().Authentication.SignOut(
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                    CookieAuthenticationDefaults.AuthenticationType);
+        }
+    }
+}
